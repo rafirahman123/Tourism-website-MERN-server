2880ed6e4d601d96447e5856f4aa70a48d44ac94
rafirahman123 <76124938+rafirahman123@users.noreply.github.com>
2021-10-31 22:38:42 +0600

    some comment remove just

--- index.js
+++ index.js
@@ -10,8 +10,7 @@ const ObjectId = require("mongodb").ObjectId;
 app.use(cors());
 app.use(express.json());
 
-// tourismUser
-// B5FR66laauY80F3z
+
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.3fgg4.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
